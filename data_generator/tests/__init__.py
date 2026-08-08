ef838ccf60968349fda6311fb6dbd7a2086f8856
Dirk Colbry <colbrydi@msu.edu>
2026-08-08 19:05:22 -0400

    Cleaning up

--- data_generator/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Test package for data_generator.
-
-Overview:
-- Purpose: Mark this directory as a Python test package.
-- Used by: Python import/test discovery behavior.
-- Adds: Predictable test discovery structure.
-- Learn more: https://docs.pytest.org/en/stable/explanation/goodpractices.html
-"""
